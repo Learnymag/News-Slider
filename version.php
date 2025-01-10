90be84e7aeccec29345c2d74ffbc3417f5a2cbde
YMAG-StephenBornet <s.bornet@ymag.fr>
2025-01-10 07:52:09 +0100

    Change version for submission

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_newsslider';
-$plugin->release = '0.1.0';
+$plugin->release = '0.2.0';
 $plugin->version = 2024070302;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
